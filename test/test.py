4a2622e49b162bbcb7551038bbeb4413581f5862
Philip Pemberton <philpem@philpem.me.uk>
2010-10-28 13:08:59 +0100

    add latest test.py

--- test/test.py
+++ test/test.py
@@ -2,7 +2,77 @@
 
 # DiscFerret ATE code, based on code by Julius Constante
 
-import sys, usb, struct, datetime
+import sys, usb, struct, datetime, random, time
+
+#############################################################################
+
+# Control registers (write only)
+DRIVE_CONTROL			= 0x04
+ACQCON					= 0x05
+ACQ_START_EVT			= 0x06
+ACQ_STOP_EVT			= 0x07
+ACQ_HSTMD_THR_START		= 0x08
+ACQ_HSTMD_THR_STOP		= 0x09
+MFM_SYNCWORD_START_L	= 0x0A
+MFM_SYNCWORD_START_H	= 0x0B
+MFM_SYNCWORD_STOP_L		= 0x0C
+MFM_SYNCWORD_STOP_H		= 0x0D
+STEP_RATE				= 0x0E	# step rate, 250us per count
+STEP_CMD				= 0x0F	# step command, bit7=direction, rest=num steps
+
+# Status registers (read only)
+STATUS1					= 0x0E
+STATUS2					= 0x0F
+
+# -----
+# Step Command bits
+STEP_CMD_TOWARDS_ZERO	= 0x80
+STEP_CMD_AWAYFROM_ZERO	= 0x00
+STEP_COUNT_MASK			= 0x7F
+
+# -----
+# DRIVE_CONTROL bits
+DRIVE_CONTROL_DENSITY	= 0x01
+DRIVE_CONTROL_INUSE		= 0x02
+DRIVE_CONTROL_DS0		= 0x04
+DRIVE_CONTROL_DS1		= 0x08
+DRIVE_CONTROL_DS2		= 0x10
+DRIVE_CONTROL_DS3		= 0x20
+DRIVE_CONTROL_MOTEN		= 0x40
+DRIVE_CONTROL_SIDESEL	= 0x80
+
+# -----
+# ACQCON bits
+ACQCON_ABORT			= 0x02
+ACQCON_START			= 0x01
+ACQCON_RATE_1MBPS		= (0x00 << 6)
+ACQCON_RATE_500KBPS		= (0x01 << 6)
+ACQCON_RATE_250KBPS		= (0x02 << 6)
+ACQCON_RATE_125KBPS		= (0x03 << 6)
+
+# -----
+# masks and events for ACQ_*_EVT registers
+ACQ_NUM_MASK			= 0x1f
+ACQ_EVENT_IMMEDIATE		= 0
+ACQ_EVENT_INDEX			= (0x01 << 5)
+ACQ_EVENT_MFM			= (0x02 << 5)
+# "wait for HSTMD" combination bit
+ACQ_HSTMD				= (0x04 << 5)
+
+# -----
+# Status bits
+STATUS1_ACQSTATUS_MASK	= 0x03
+STATUS1_ACQ_WAITING		= 0x02
+STATUS1_ACQ_ACQUIRING	= 0x01
+STATUS1_ACQ_IDLE		= 0x00
+STATUS2_INDEX			= 0x80
+STATUS2_TRACK0			= 0x40
+STATUS2_WRITE_PROTECT	= 0x20
+STATUS2_DISC_CHANGE		= 0x10
+STATUS2_DENSITY			= 0x08
+STATUS2_STEPPING		= 0x04
+STATUS2_RAM_EMPTY		= 0x02
+STATUS2_RAM_FULL		= 0x01
 
 #############################################################################
 
@@ -60,13 +130,13 @@ class DiscFerret:
 		except:
 			return False
 
-	def write(self, ep, buff, timeout = 100):
+	def write(self, ep, buff, timeout = 1000):
 		try:
 			return self.handle.bulkWrite(ep, buff, timeout) #return bytes written
 		except:
 			return 0
 
-	def read(self, ep, size, timeout = 100):
+	def read(self, ep, size, timeout = 1000):
 		try:
 			# the OR 0x80 forces a READ
 			return self.handle.bulkRead(ep | 0x80, size, timeout) # return data read
@@ -158,7 +228,7 @@ class DiscFerret:
 		return resp[0]
 
 	def ramRead(self, nbytes):
-		# TODO: raise exception if nbytes > 63
+		# TODO: raise exception if nbytes > 64
 		packet = [CMD_RAM_READ, nbytes & 0xff, (nbytes >> 8) & 0xff]
 		self.write(1, packet)
 		resp = self.read(0x81, nbytes+1)
@@ -166,6 +236,42 @@ class DiscFerret:
 			return resp[1:]
 		else:
 			return False
+
+	def debug_dump_status(self):
+		a = self.peek(STATUS1)
+		b = self.peek(STATUS2)
+		# STATUS1
+		if (a & STATUS1_ACQSTATUS_MASK) == STATUS1_ACQ_WAITING:
+			s = "waiting, "
+		elif (a & STATUS1_ACQSTATUS_MASK) == STATUS1_ACQ_ACQUIRING:
+			s = "acquiring, "
+		elif (a & STATUS1_ACQSTATUS_MASK) == STATUS1_ACQ_IDLE:
+			s = "idle, "
+		else:
+			s = "ILLEGAL, "
+
+		# STATUS2
+		if (b & STATUS2_INDEX):
+			s = s + "INDEX "
+		if (b & STATUS2_TRACK0):
+			s = s + "TRACK0 "
+		if (b & STATUS2_WRITE_PROTECT):
+			s = s + "WRPROT "
+		if (b & STATUS2_DISC_CHANGE):
+			s = s + "DISC-CHANGE "
+		if (b & STATUS2_DENSITY):
+			s = s + "DENSITY "
+		if (b & STATUS2_STEPPING):
+			s = s + "STEPPING "
+		if (b & STATUS2_RAM_EMPTY):
+			s = s + "RAM-EMPTY "
+		if (b & STATUS2_RAM_FULL):
+			s = s + "RAM-FULL "
+		if s[-1] == ' ':
+			s = s[:-1]
+		if s[-1] == ',':
+			s = s[:-1]
+		print "STATUS: %02X %02X [%s]" % (a,b,s)
 
 #############################################################################
 
@@ -187,6 +293,7 @@ if not dev.open():
 else:
 	print "Device opened successfully"
 
+"""
 print "Initialising FPGA...",
 # FPGA LOAD INIT -- start a microcode load
 resp = dev.fpgaLoadBegin()
@@ -240,7 +347,7 @@ while pos < len(rbf):
 
 	# update pointer
 	pos += i
-
+"""
 # poll fpga status
 print "Load complete. FPGA status:",
 resp = dev.fpgaGetLoadStatus()
@@ -253,14 +360,186 @@ else:
 	print "FPGA status code unknown, is %d, wanted %d or %d" % (resp, ERR_OK, ERR_FPGA_NOT_CONF)
 	sys.exit(-1)
 
+"""
 print dev.getDeviceInfo()
-print "current ram address: %06X" % dev.getRAMAddr()
+print "current ram address: 0x%06X" % dev.getRAMAddr()
 print "set addr to zero, resp: %d" % dev.setRAMAddr(0)
-print "read 32 byte block:\n   ",
-print dev.ramRead(32)
-print "set addr to zero, resp: %d" % dev.setRAMAddr(0)
-print "write incrementing sequence: %d" % dev.ramWrite(range(32))
-print "set addr to zero, resp: %d" % dev.setRAMAddr(0)
-print "read 32 byte block:\n   ",
-print dev.ramRead(32)
+print "current ram address: 0x%06X" % dev.getRAMAddr()
+print "read 40 byte block:\n   ",
+print dev.ramRead(40)
+print "current ram address: 0x%06X" % dev.getRAMAddr()
+print
 
+print "set addr to zero, resp: %d" % dev.setRAMAddr(0)
+print "current ram address: 0x%06X" % dev.getRAMAddr()
+print "write incrementing sequence: %d" % dev.ramWrite(range(32))
+print "  ",
+print range(32)
+print "current ram address: 0x%06X" % dev.getRAMAddr()
+print
+
+# POKE DB: Poke DeBug. Used to trigger SignalTap.
+print "poke 0xDB=0x55: %d" % dev.poke(0xDB, 0x55)
+print "current ram address: 0x%06X" % dev.getRAMAddr()
+print "set addr to zero, resp: %d" % dev.setRAMAddr(0)
+print "current ram address: 0x%06X" % dev.getRAMAddr()
+print "read 40 byte block:\n   ",
+print dev.ramRead(40)
+print "current ram address: 0x%06X" % dev.getRAMAddr()
+print
+
+print "set addr to zero, resp: %d" % dev.setRAMAddr(0)
+print "current ram address: 0x%06X" % dev.getRAMAddr()
+a = list()
+for i in range(32):
+	a.append(int(random.getrandbits(8)))
+print "write random sequence: %d" % dev.ramWrite(a)
+print "  ",
+print a
+print "current ram address: 0x%06X" % dev.getRAMAddr()
+print
+
+# POKE DB: Poke DeBug. Used to trigger SignalTap.
+print "poke 0xDB=0x55: %d" % dev.poke(0xDB, 0x55)
+print "current ram address: 0x%06X" % dev.getRAMAddr()
+print "set addr to zero, resp: %d" % dev.setRAMAddr(0)
+print "current ram address: 0x%06X" % dev.getRAMAddr()
+print "read 40 byte block:\n   ",
+print dev.ramRead(40)
+print "current ram address: 0x%06X" % dev.getRAMAddr()
+print
+"""
+
+# reset ram address
+print "set ram addr to zero, resp: %d" % dev.setRAMAddr(0)
+# get hardware status
+dev.debug_dump_status()
+print
+
+# abort any currently running acquisition
+print "abort acquisition: %d" % dev.poke(ACQCON, ACQCON_ABORT)
+dev.debug_dump_status()
+print
+
+acqcon_v = 0
+"""
+# Demo of MFM-synched start/stop for 3.5in DSDD (720K) discs
+# set start/stop sync words to 0x4489
+print "set start mfm hi: resp %d" % dev.poke(MFM_SYNCWORD_START_H, 0x44)
+print "set start mfm lo: resp %d" % dev.poke(MFM_SYNCWORD_START_L, 0x89)
+print "set stop  mfm hi: resp %d" % dev.poke(MFM_SYNCWORD_STOP_H,  0x44)
+print "set stop  mfm lo: resp %d" % dev.poke(MFM_SYNCWORD_STOP_L,  0x89)
+print "set start event: resp %d" % dev.poke(ACQ_START_EVT, ACQ_EVENT_MFM | (1 & ACQ_NUM_MASK))
+print "set start event: resp %d" % dev.poke(ACQ_STOP_EVT, ACQ_EVENT_MFM | (31 & ACQ_NUM_MASK))
+acqcon_v = acqcon | ACQCON_RATE_250KBPS
+print "set mfm clock rate: resp %d" % dev.poke(ACQCON, acqcon_v)
+"""
+
+# start event: index pulse, second instance
+#print "set start event: resp %d" % dev.poke(ACQ_START_EVT, ACQ_EVENT_INDEX | (1 & ACQ_NUM_MASK))
+# stop event: index pulse, first instance
+#print "set stop event: resp %d" % dev.poke(ACQ_STOP_EVT, ACQ_EVENT_INDEX | (0 & ACQ_NUM_MASK))
+# HSTMD thresholds don't need to be set
+# set step rate to 6ms
+print "set step rate: resp %d" % dev.poke(STEP_RATE, 6000/250)
+
+# select drive 0 (PC cable -- DS2=MOTEN iirc)
+print "select: resp %d" % dev.poke(DRIVE_CONTROL, DRIVE_CONTROL_DS2 | DRIVE_CONTROL_DS0 | DRIVE_CONTROL_SIDESEL)
+time.sleep(3)
+# seek to track zero
+print "seek to zero: resp %d" % dev.poke(STEP_CMD, STEP_CMD_TOWARDS_ZERO | (90 & STEP_COUNT_MASK))
+dev.debug_dump_status()
+stat = STATUS2_STEPPING
+while (stat & STATUS2_STEPPING) != 0:
+	stat = dev.peek(STATUS2)
+dev.debug_dump_status()
+# seek to track 4
+"""
+print "seek to 4: resp %d" % dev.poke(STEP_CMD, STEP_CMD_AWAYFROM_ZERO | (4 & STEP_COUNT_MASK))
+dev.debug_dump_status()
+stat = STATUS2_STEPPING
+while (stat & STATUS2_STEPPING) != 0:
+	stat = dev.peek(STATUS2)
+dev.debug_dump_status()
+"""
+# head settling time
+time.sleep(1)
+
+# start acquisition
+print "start acq: resp %d" % dev.poke(ACQCON, acqcon_v | ACQCON_START)
+# dump status
+dev.debug_dump_status()
+
+# wait for acquisition to start
+stat = 0
+while (stat & STATUS1_ACQSTATUS_MASK) != STATUS1_ACQ_ACQUIRING:
+	stat = dev.peek(STATUS1)
+print "acquisition started."
+dev.debug_dump_status()
+stat = STATUS1_ACQ_ACQUIRING
+while (stat & STATUS1_ACQSTATUS_MASK) == STATUS1_ACQ_ACQUIRING:
+	stat = dev.peek(STATUS1)
+print "acquisition completed."
+dev.debug_dump_status()
+nbytes = dev.getRAMAddr()-1
+print "acquired %d bytes of data" % (nbytes)
+
+# now save the RAM data
+dev.setRAMAddr(0)
+lp = nbytes
+ramdata = []
+while lp > 0:
+	if lp >= 63:
+		data = dev.ramRead(63)
+		lp = lp - 63
+	else:
+		data = dev.ramRead(lp)
+		lp = 0
+	ramdata.extend(data)
+
+f=open("dat.bin", "wb")
+g=open("dat", "wt")
+h=open("dat.his", "wt")
+s=""
+# binary dump
+for x in ramdata:
+	s = s + struct.pack('B', x)
+f.write(s)
+f.close()
+
+# text dump
+s=""
+carry=0
+pos=0
+cdat=[]
+for x in ramdata:
+	if (x&0x7f)==0:
+		carry += 128
+	else:
+		s = s + ("%d %d\n" % (pos, carry + (x & 0x7f)))
+		cdat.append(carry + (x & 0x7f))
+		carry = 0
+		pos = pos + 1
+g.write(s)
+g.close()
+
+# histogram
+s=""
+maxv=0
+for x in cdat:
+	if (x > maxv):
+		maxv = x
+histogram=[0]*(maxv+1)
+for x in cdat:
+	histogram[x] = histogram[x] + 1
+pos = 0
+for x in histogram:
+	s = s + ("%d %d\n" % (pos, x))
+	pos = pos + 1
+h.write(s)
+h.close()
+
+# deselect all drives
+print "deselect: resp %d" % dev.poke(DRIVE_CONTROL, 0)
+
+print
